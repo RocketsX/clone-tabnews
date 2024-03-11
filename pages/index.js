1e46e4dff987be906324b483d32470bed0d62cf3
RocketsX <156356000+RocketsX@users.noreply.github.com>
2024-03-11 14:36:29 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>Eu te amo Filha 🤯 </h1>
+function Home() {
+  return <h1>Eu te amo Filha 🤯 </h1>;
+}
+
+function test() {
+  console.log("teste");
+}
+
+function test2() {
+  console.log("indentação errada");
 }
 
 export default Home;
