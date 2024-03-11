f59801c3c032b09041c5874f32dc54a543d5b2df
RocketsX <156356000+RocketsX@users.noreply.github.com>
2024-03-11 21:00:16 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,4 @@
 function Home() {
   return <h1>Eu te amo Filha 🤯 </h1>;
 }
-
-function test() {
-  console.log("teste");
-}
-
-function test2() {
-  console.log("indentação errada");
-}
-
 export default Home;
